e0973f683f38ef38b24f1ba1dc3e4f6a175cd3e6
PC680 <hasanboronov@gmail.com>
2023-02-24 17:09:48 +0500

    0523

--- app/src/main/java/uz/itschool/buyurtmaberish/MainActivity.java
+++ app/src/main/java/uz/itschool/buyurtmaberish/MainActivity.java
@@ -1,3 +1,4 @@
+
 package uz.itschool.buyurtmaberish;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,46 +30,47 @@ public class MainActivity extends AppCompatActivity {
     public void buyurtmaBerish() {
         checkBox = (CheckBox) findViewById(R.id.checkBox);
         checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
-        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox2 = findViewById(R.id.checkBox2);
         checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
         checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                                      @Override
+                                      public void onClick(View view) {
 
-                StringBuilder natija = new StringBuilder();
-                natija.append("\nTanlangan taomlar:");
-                int jami = 0;
+                                          StringBuilder natija = new StringBuilder();
+                                          natija.append("\nTanlangan taomlar:");
+                                          int jami = 0;
 
-                if (checkBox.isChecked()) {
-                    natija.append(" \nHamburger 15 000 so'm ");
-                    jami += 15000;
-                }
+                                          if (checkBox.isChecked()) {
+                                              natija.append(" \nHamburger 15 000 so'm ");
+                                              jami += 15000;
+                                          }
 
-                if (checkBox1.isChecked()) {
-                    natija.append("\nSandwich 10 000 so'm");
-                    jami += 10000;
-                }
-                if (checkBox2.isChecked()) {
-                    natija.append("\nHotdog5 000");
-                    jami += 5000;
-                }
-                if (checkBox3.isChecked()) {
-                    natija.append("\nCoca_cola 3 000 so'm");
-                    jami += 3000;
-                }
-                if (checkBox4.isChecked()) {
-                    natija.append("\nSharbat  2 000 so'm ");
-                    jami += 2000;
-                }
-                natija.append("\nUmumiy to'lov:" + jami + "so'm");
+                                          if (checkBox1.isChecked()) {
+                                              natija.append("\nSandwich 10 000 so'm");
+                                              jami += 10000;
+                                          }
+                                          if (checkBox2.isChecked()) {
+                                              natija.append("\nHotdog5 000");
+                                              jami += 5000;
+                                          }
+                                          if (checkBox3.isChecked()) {
+                                              natija.append("\nCoca_cola 3 000 so'm");
+                                              jami += 3000;
+                                          }
+                                          if (checkBox4.isChecked()) {
+                                              natija.append("\nSharbat  2 000 so'm ");
+                                              jami += 2000;
+                                          }
+                                          natija.append("\nUmumiy to'lov:" + jami + "so'm");
 
-                Toast.makeText(MainActivity.this, natija.toString(), Toast.LENGTH_LONG).show();
+                                          Toast.makeText(MainActivity.this, natija.toString(), Toast.LENGTH_LONG).show();
 
-            }
-        })
+                                      }
+
+                                  }
+        );
     }
 }
-
